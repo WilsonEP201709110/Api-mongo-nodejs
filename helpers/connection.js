4515b2f4b211a7348b1c3d493acdd8ca05bd0d73
Wilson Perez <3038188240110@ingenieria.usac.edu.gt>
2023-12-30 17:27:08 -0600

    cc

--- helpers/connection.js
+++ helpers/connection.js
@@ -4,9 +4,7 @@ let db = null;
 
 async function connectDatabase() {
   try {
-    const client = new MongoClient('mongodb+srv://grupo15:grupo15@proyectodb2.skqlwup.mongodb.net/?retryWrites=true&w=majority', {
-      useNewUrlParser: true
-    });
+    const client = new MongoClient('mongodb+srv://grupo15:grupo15@proyectodb2.skqlwup.mongodb.net/?retryWrites=true&w=majority');
     await client.connect();
     db = client.db('mi_base_de_datos');
     console.log('Conexión a MongoDB establecida');
